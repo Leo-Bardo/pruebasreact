66b66f0243c444873c65d4016469153e150e63a5
Leo-Bardo <julioalb4488@gmail.com>
2024-02-09 09:25:33 -0600

    Traer datos a WO

--- src/components/primerComponente.js
+++ src/components/primerComponente.js
@@ -1,5 +1,13 @@
 import React from 'react'
-// No contiene export const con rafc como en el tutorial 
+// No contiene export const con rafc como en el tutorial
+import React from 'react'
+
+export const segundoComponente = () => {
+  return (
+    <div>segundoComponente</div>
+  )
+}
+
 const primerComponente = () => {
   return (
     <div>
